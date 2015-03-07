6342f352b19b209ad06af80042ceb8493b3f9757
Hasta Ragil <npcmamaz@gmail.com>
2015-03-07 16:54:27 +0700

    Add ide helper

--- config/app.php
+++ config/app.php
@@ -136,6 +136,8 @@ return [
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 
+        'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
+
 		/*
 		 * Application Service Providers...
 		 */
