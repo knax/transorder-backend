a654d916a7e3670de5ec9b4c2c55796197ae968e
Hasta Ragil <npcmamaz@gmail.com>
2015-03-08 15:54:39 +0700

    Add main layout

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Transorder</title>
+    <link rel="stylesheet" href="/css/app.css"/>
+</head>
+<body>
+
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</body>
+</html>
