e10c12eba9b6b1aa5f8a7db9128f8b0d1a359942
Hasta Ragil <npcmamaz@gmail.com>
2015-03-08 17:56:40 +0700

    Add api for product controller

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,12 @@
+<?php namespace Transorder\Http\Controllers\Api;
+
+use Transorder\Http\Requests;
+use Transorder\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller {
+
+	//
+
+}
