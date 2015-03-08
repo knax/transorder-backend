e95cc0a045a3ab3309087828eabe2f2f702b67b0
Hasta Ragil <npcmamaz@gmail.com>
2015-03-08 20:15:32 +0700

    Add order seeder to seeder list

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,21 +1,23 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder {
+class DatabaseSeeder extends Seeder
+{
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Model::unguard();
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
 
-		 $this->call('ProductSeeder');
-		 $this->call('UserSeeder');
-	}
+        $this->call('ProductSeeder');
+        $this->call('UserSeeder');
+        $this->call('OrderSeeder');
+    }
 
 }
